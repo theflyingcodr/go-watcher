49523d9ca1dc095e209213d70ecfd5c2945596c7
Mark Smith <mark@theflyingcodr.dev>
2021-02-15 22:35:55 +0000

    removing gvm from docker file, now pinning image tags per go version

--- docker-examples/hello/main.go
+++ docker-examples/hello/main.go
@@ -9,7 +9,7 @@ func main() {
 	http.ListenAndServe(":7000",
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintln(w, "watcher is running")
+				fmt.Fprintln(w, "watcher is running again")
 			},
 		),
 	)
